bc98796f6ea074a4b9c9617c5edde11cd2adb4f9
Rahaf Issam <rahaf.issam2007@gmail.com>
2026-01-18 17:38:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -464,7 +464,7 @@ function renderContact() {
                         height="100%" 
                         frameborder="0" 
                         style="border:0" 
-                        src="https://maps.app.goo.gl/RGqZQrXnEp9SALG59" 
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3624.123456789!2d46.123456789!3d24.123456789!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjTCsDA3JzI0LjUiTiA0NsKwMDcnMjQuNSJF!5e0!3m2!1sen!2ssa!4v1234567890" 
                         allowfullscreen>
                     </iframe>
                 </div>
